fd367737f515aa6c2d1a31986f65293af584850a
nikhilsonilctwn <nikhilkamlesh.soni@gmail.com>
2022-05-19 12:42:35 -0700

    changed database migration and seed data structure

--- pincode_service/migrations/alembic/alembic/env.py
+++ pincode_service/migrations/alembic/alembic/env.py
@@ -1,8 +1,6 @@
 from logging.config import fileConfig
-
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
 from alembic import context
 import os
 
@@ -26,7 +24,6 @@ target_metadata = None
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 config.set_main_option("sqlalchemy.url", os.getenv("DATABASE_URI"))
-
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
@@ -66,7 +63,9 @@ def run_migrations_online():
     )
 
     with connectable.connect() as connection:
-        context.configure(connection=connection, target_metadata=target_metadata)
+        context.configure(
+            connection=connection, target_metadata=target_metadata
+        )
 
         with context.begin_transaction():
             context.run_migrations()
